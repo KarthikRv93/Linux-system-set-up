93c243cc0a5be32ad53f177382cd15e81b0f90f9
karthik <karthik.rv93@gmail.com>
2018-12-29 14:56:28 -0700

    installation v2

--- installations.sh
+++ installations.sh
@@ -4,6 +4,7 @@ sudo apt-get --purge remove git-core
 sudo apt-get --purge remove vim
 sudo apt-get --purge remove curl
 
+sudo apt-get install git
 sudo apt-get install zsh
 sudo apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
@@ -18,4 +19,13 @@ sudo apt-get install curl
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+git clone https://github.com/KarthikRv93/Linux-system-set-up.git
+cp ~/Linux-system-set-up/.vimrc ~/
+git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/ervandew/supertab ~/.vim/bundle/supertab
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/chrisbra/vim-commentary ~/.vim/bundle/vim-commentary
+git clone git://github.com/nathanaelkane/vim-indent-guides.git ~/.vim/bundle/vim-indent-guides
 sudo shutdown -r 0
